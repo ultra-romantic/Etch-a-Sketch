e77136c732cfdb3cceb3378f1ad18006fa78251e
ultra-romantic <shiftybroccoli74@gmail.com>
2023-02-20 03:22:56 -0300

    add function createSquare, createGrid working properly

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-for(let i=0;i<(16*16);i++){
+let scale=16;
+
+function createSquare(){
     const gridSquare = document.createElement("div");
     gridSquare.classList.add("square");
     gridSquare.style.backgroundColor = "white";
@@ -6,6 +8,55 @@ for(let i=0;i<(16*16);i++){
     gridSquare.addEventListener('mouseenter', () => {
         gridSquare.style.backgroundColor="black";
     });
+    return gridSquare;
+}
+
+function createGrid(){
     const gridContainer = document.getElementById("grid-container");
-    gridContainer.appendChild(gridSquare);
-};
+    gridContainer.style.gridTemplateColumns = `repeat(${scale}, auto)`;
+    gridContainer.style.gridTemplateRows = `repeat(${scale}, auto)`;
+    for(let i=0;i<(scale*scale);i++){
+        gridContainer.appendChild(createSquare());
+    };
+}
+
+/*function askNewScale(){
+    let newScale=prompt("Please enter a number from 1 to 100", "");
+    parseInt(newScale, 10);
+    if(isNaN(newScale)===false){
+        if((newScale<=100) || (newScale>=1)){
+            return newScale;
+        }else {
+            alert("Must be between 1 - 100");
+        }return false;
+    }else if (isNaN(newScale)){
+        alert("Must be a numeric value");
+        return false;
+    }
+}
+
+function gridDelete(grid){
+    for(let i=0;i<(scale*scale);i++){
+        grid.remove();
+    };
+}*/
+
+createGrid();
+
+/*const changeGridBtn = document.getElementById("change-grid");
+changeGridBtn.addEventListener('click', e => {
+    const gridContainer = document.getElementById("grid-container");
+    let a=askNewScale();
+    console.log(a);
+    gridDelete(gridContainer);
+    createGrid();
+})*/
+
+/*
+function generateNewGrid(n){
+    for(let i=0;i<(n*n);i++){
+        const gridSquare = document.createElement("div");
+        gridSquare.remove();
+    };
+
+*/
